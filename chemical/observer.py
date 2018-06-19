2863d974a0ef9f07ae4273d68e5389620048a4ce
Robert Heedt <robert.heedt@gmail.com>
2018-06-19 13:47:17 +0200

    Add zero clipping to chemical EKF Add analysis notebook to chemical More quadcopter experiments

--- chemical/observer.py
+++ chemical/observer.py
@@ -6,9 +6,21 @@ from chemical.model import ChemicalObserverModel
 
 
 class ChemicalEKF(ExtendedKalmanFilterObserver):
+    @staticmethod
+    def add_public_settings():
+        ChemicalEKF.public_settings['zero clipping'] = False
+
     def __init__(self, settings):
         np.random.seed(0)
         super().__init__(settings, ChemicalObserverModel())
+        self.zero_clipping = self._settings['zero clipping']
+
+    def _observe(self, time, system_input, system_output):
+        obs_result = super()._observe(time, system_input, system_output)
+        if self.zero_clipping:
+            self.filter_algorithm.x = np.fmax(self.filter_algorithm.x, np.zeros(2, dtype=float))
+
+        return self.filter_algorithm.x
 
 
 class ChemicalMHE(MovingHorizonEstimator):
@@ -17,5 +29,6 @@ class ChemicalMHE(MovingHorizonEstimator):
         super().__init__(settings, ChemicalObserverModel())
 
 
+ChemicalEKF.add_public_settings()
 pm.register_simulation_module(pm.Observer, ChemicalEKF)
 pm.register_simulation_module(pm.Observer, ChemicalMHE)
